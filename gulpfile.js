0acb6c0bd14c30dba117904df1934eadbbace269
Seven Lju <dna2gle@gmail.com>
2016-06-25 23:08:33 +0800

    [gulp] automatic prepare
    - copy basic library files

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp'),
+    notify = require("gulp-notify"),
+    bower_install = require('gulp-bower');
+
+var config = {
+  bowerDir: function (path) {
+    return './bower_components' + (path || '');
+  },
+  staticDir: function (path) {
+    return './static' + (path || '');
+  }
+}
+
+gulp.task('bower_install', function () {
+  bower_install().pipe(
+    gulp.dest(config.bowerDir())
+  );
+});
+
+gulp.task('bower_deploy', function () {
+  var lib_files = [
+    config.bowerDir('/select2/dist/js/select2.min.js'),
+    config.bowerDir('/bootstrap/dist/js/bootstrap.min.js'),
+    config.bowerDir('/jquery/dist/jquery.min.js'),
+    config.bowerDir('/select2/dist/css/select2.min.css'),
+    config.bowerDir('/bootstrap/dist/css/bootstrap.min.css'),
+    config.bowerDir('/font-awesome/css/font-awesome.min.css')
+  ];
+  gulp.src(lib_files)
+    .pipe(gulp.dest(config.staticDir('/lib')));
+
+  var font_files = [
+    config.bowerDir('/font-awesome/fonts/fontawesome-webfont.woff2'),
+    config.bowerDir('/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2'),
+    config.bowerDir('/font-awesome/fonts/fontawesome-webfont.svg'),
+    config.bowerDir('/bootstrap/dist/fonts/glyphicons-halflings-regular.svg')
+  ]
+  gulp.src(font_files)
+    .pipe(gulp.dest(config.staticDir('/fonts')));
+});
+
+gulp.task('default', ['bower_install', 'bower_deploy']);
